e6844c1311dd5b8aba84347abb168f529b2049e6
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-11 02:57:43 +0600

    feat: add coingecko endpoints

---
+++ src/api/coingecko/endpoints.ts
@@ -0,0 +1,18 @@
+import type { AxiosPromise } from "axios";
+import instance from "./instance";
+
+export interface ApiInterface {
+  trending: () => AxiosPromise<TrendingResponse>;
+  search: (query: string) => AxiosPromise<SearchResponse>;
+}
+
+const API: ApiInterface = {
+  trending() {
+    return instance.get("search/trending");
+  },
+  search(query) {
+    return instance.get("search", { params: query });
+  },
+};
+
+export default API;
