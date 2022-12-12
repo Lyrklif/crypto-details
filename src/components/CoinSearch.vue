0c513b866cf042a656c0f2f5d65cbdc04b4b03cd
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-12 06:41:55 +0600

    feat: home view

---
+++ src/components/CoinSearch.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import API from "../api";
+
+const coins = ref([]);
+
+// TODO uncomment
+// async function load() {
+//   const response = await API.coingecko.trending();
+//   console.log("TTT response", response);
+// }
+// load();
+</script>
+
+<template>
+  <form>
+    <div class="form-group">
+      <label class="input-group mb-4" for="coin-name">
+        <input id="coin-name" type="text" class="form-control" />
+      </label>
+    </div>
+
+    <ul v-if="coins.length" class="flex-row">
+      <li v-for="item in coins" :key="item.id">
+        <button
+          type="button"
+          class="btn btn-primary btn-pill text-dark mr-1 mb-1"
+        >
+          {{ item.name }}
+        </button>
+      </li>
+    </ul>
+
+    <button type="submit" class="btn btn-block btn-primary">Поиск</button>
+  </form>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import CoinSearch from "@/components/CoinSearch.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="row justify-content-center mt-7">
+    <div class="col-12 col-md-8 col-lg-6 justify-content-center">
+      <div class="card bg-primary shadow-soft border-light p-4">
+        <div class="card-header text-center pb-0">
+          <h2 class="h4">Введите название криптовалюты</h2>
+        </div>
+        <div class="card-body">
+          <CoinSearch />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
