d9c6aa8324643d8ed758e3c6b9f521d9aa1c1773
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-17 11:29:38 +0600

    feat: add PriceWidget

---
+++ src/components/PriceWidget.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+defineProps({
+  id: String as PropType<string>,
+});
+</script>
+
+<template>
+  <iframe
+    class="block"
+    :src="`https://coinpaprika.com/coin/${id}/embed/?interval=0&modules[]=market_details&modules[]=chart&primaryCurrency=USD`"
+    width="350"
+  ></iframe>
+</template>
+
+<style scoped>
+.block {
+  width: 100%;
+  height: 630px;
+  filter: saturate(0.4);
+  border: none;
+}
+</style>
