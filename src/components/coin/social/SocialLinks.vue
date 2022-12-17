3f834906aa710683b626d4738d4c82f75a507f47
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-17 07:09:29 +0600

    feat: add SocialLinks component

---
+++ src/components/coin/social/SocialLinks.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+import type { LinkExtended } from "../../../api/coinpaprika/types";
+import SocialLink from "./SocialLink.vue";
+
+defineProps({
+  links: Array as PropType<Array<LinkExtended>>,
+});
+</script>
+
+<template>
+  <ul class="list-unstyled d-flex flex-wrap">
+    <li v-for="(item, index) in links" :key="index" class="mb-1 mr-2">
+      <SocialLink :item="item" />
+    </li>
+  </ul>
+</template>
