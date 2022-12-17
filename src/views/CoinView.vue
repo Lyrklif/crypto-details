1854961ea33e60576efef2187d5e1fb299b5a2c3
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-17 12:45:24 +0600

    feat: add ExchangeList component

--- src/views/CoinView.vue
+++ src/views/CoinView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import CoinBase from "../components/coin/CoinBase.vue";
 import TwitterList from "../components/coin/TwitterList.vue";
+import ExchangeList from "../components/ExchangeList.vue";
 </script>
 
 <template>
   <div>
     <CoinBase />
-    <TwitterList />
+    <TwitterList class="mb-5" />
+    <ExchangeList />
   </div>
 </template>
