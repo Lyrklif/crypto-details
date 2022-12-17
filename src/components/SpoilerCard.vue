116c808454e40fb0b75df7a28533ab7182ee5067
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-17 12:44:46 +0600

    feat: add SpoilerCard component

---
+++ src/components/SpoilerCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { PropType, ref } from "vue";
+
+const isShow = ref(false);
+
+defineProps({
+  title: String as PropType<string>,
+  content: String as PropType<string>,
+});
+</script>
+
+<template>
+  <div>
+    <button
+      class="accordion-panel-header collapsed btn btn-block py-3 accordion-panel-header animate-down-2"
+      :aria-expanded="isShow"
+      @click="isShow = !isShow"
+    >
+      <span class="h6 mb-0 font-weight-bold">
+        {{ title }}
+      </span>
+      <span class="icon"><span class="fas fa-plus"></span></span>
+    </button>
+    <div
+      class="collapse card-body bg-primary shadow-inset border-light rounded"
+      :class="{ show: isShow }"
+    >
+      <div class="pt-3">
+        <slot name="content">
+          <p class="mb-0">
+            {{ content }}
+          </p>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
