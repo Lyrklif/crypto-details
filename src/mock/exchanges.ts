77c32ece0a4e0b615e9de5ff4608a66064756494
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-17 12:45:07 +0600

    feat: exchanges mock

---
+++ src/mock/exchanges.ts
@@ -0,0 +1,733 @@
+export default [
+  {
+    id: "binance",
+    name: "Binance",
+    adjusted_volume_24h_share: 45.07300801,
+    fiats: [
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+      {
+        name: "Pound Sterling",
+        symbol: "GBP",
+      },
+      {
+        name: "Polish Zloty",
+        symbol: "PLN",
+      },
+      {
+        name: "Russian Ruble",
+        symbol: "RUB",
+      },
+      {
+        name: "Turkish Lira",
+        symbol: "TRY",
+      },
+      {
+        name: "Australian Dollar",
+        symbol: "AUD",
+      },
+      {
+        name: "Ukrainian Hryvnia",
+        symbol: "UAH",
+      },
+      {
+        name: "Nigerian Naira",
+        symbol: "NGN",
+      },
+      {
+        name: "South African Rand",
+        symbol: "ZAR",
+      },
+    ],
+  },
+  {
+    id: "zedxion-exchange",
+    name: "ZEDXION EXCHANGE",
+    adjusted_volume_24h_share: 16.71954097,
+    fiats: [],
+  },
+  {
+    id: "hitbtc",
+    name: "HitBTC",
+    adjusted_volume_24h_share: 7.21441047,
+    fiats: [],
+  },
+  {
+    id: "p2b",
+    name: "P2B",
+    adjusted_volume_24h_share: 6.70746867,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "coinsbit",
+    name: "Coinsbit",
+    adjusted_volume_24h_share: 6.2001737,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "indoex",
+    name: "Indoex",
+    adjusted_volume_24h_share: 2.70577993,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "bkex",
+    name: "Bkex",
+    adjusted_volume_24h_share: 2.44836427,
+    fiats: [],
+  },
+  {
+    id: "bitbunch",
+    name: "BitBunch",
+    adjusted_volume_24h_share: 1.85366335,
+    fiats: [],
+  },
+  {
+    id: "bitrue",
+    name: "Bitrue",
+    adjusted_volume_24h_share: 1.3447216,
+    fiats: [],
+  },
+  {
+    id: "bitmart",
+    name: "BitMart",
+    adjusted_volume_24h_share: 1.25257925,
+    fiats: [],
+  },
+  {
+    id: "kucoin",
+    name: "Kucoin",
+    adjusted_volume_24h_share: 1.07744837,
+    fiats: [
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "binance-us",
+    name: "Binance US",
+    adjusted_volume_24h_share: 0.82221073,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+    ],
+  },
+  {
+    id: "dex-trade",
+    name: "Dex-Trade",
+    adjusted_volume_24h_share: 0.79011082,
+    fiats: [],
+  },
+  {
+    id: "mexc",
+    name: "MEXC",
+    adjusted_volume_24h_share: 0.53019631,
+    fiats: [],
+  },
+  {
+    id: "fameex",
+    name: "FAMEEX",
+    adjusted_volume_24h_share: 0.53019087,
+    fiats: [],
+  },
+  {
+    id: "tokpie",
+    name: "TOKPIE",
+    adjusted_volume_24h_share: 0.49941223,
+    fiats: [],
+  },
+  {
+    id: "bitforex",
+    name: "BitForex",
+    adjusted_volume_24h_share: 0.41852458,
+    fiats: [],
+  },
+  {
+    id: "lbank",
+    name: "Lbank",
+    adjusted_volume_24h_share: 0.40366306,
+    fiats: [],
+  },
+  {
+    id: "gokumarket",
+    name: "GokuMarket",
+    adjusted_volume_24h_share: 0.34555189,
+    fiats: [
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "nexdax",
+    name: "NexDAX",
+    adjusted_volume_24h_share: 0.31906615,
+    fiats: [],
+  },
+  {
+    id: "oceanex",
+    name: "OceanEx",
+    adjusted_volume_24h_share: 0.28233851,
+    fiats: [],
+  },
+  {
+    id: "bitglobal",
+    name: "BitGlobal",
+    adjusted_volume_24h_share: 0.20771665,
+    fiats: [],
+  },
+  {
+    id: "gateio",
+    name: "Gate.io",
+    adjusted_volume_24h_share: 0.19791697,
+    fiats: [],
+  },
+  {
+    id: "pionex",
+    name: "Pionex",
+    adjusted_volume_24h_share: 0.17735084,
+    fiats: [],
+  },
+  {
+    id: "ascendex",
+    name: "AscendEX (BitMax)",
+    adjusted_volume_24h_share: 0.16185133,
+    fiats: [],
+  },
+  {
+    id: "btcex",
+    name: "BTCEX",
+    adjusted_volume_24h_share: 0.16073338,
+    fiats: [],
+  },
+  {
+    id: "koinbazar",
+    name: "Koinbazar",
+    adjusted_volume_24h_share: 0.14776458,
+    fiats: [],
+  },
+  {
+    id: "mdex",
+    name: "MDEX (BSC)",
+    adjusted_volume_24h_share: 0.12897608,
+    fiats: [],
+  },
+  {
+    id: "huobi",
+    name: "Huobi",
+    adjusted_volume_24h_share: 0.12643377,
+    fiats: [],
+  },
+  {
+    id: "bankcex",
+    name: "BankCEX",
+    adjusted_volume_24h_share: 0.12235034,
+    fiats: [],
+  },
+  {
+    id: "bitspay",
+    name: "BitsPay",
+    adjusted_volume_24h_share: 0.11322952,
+    fiats: [],
+  },
+  {
+    id: "coinamount",
+    name: "CoinAmount",
+    adjusted_volume_24h_share: 0.11192092,
+    fiats: [],
+  },
+  {
+    id: "bitbns",
+    name: "Bitbns",
+    adjusted_volume_24h_share: 0.10556801,
+    fiats: [
+      {
+        name: "Indian Rupee",
+        symbol: "INR",
+      },
+    ],
+  },
+  {
+    id: "cointiger",
+    name: "CoinTiger",
+    adjusted_volume_24h_share: 0.08059522,
+    fiats: [],
+  },
+  {
+    id: "cex-trade",
+    name: "Cex-Trade",
+    adjusted_volume_24h_share: 0.07268957,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "block-beats-exchange",
+    name: "BLOCK BEATS EXCHANGE",
+    adjusted_volume_24h_share: 0.05833969,
+    fiats: [],
+  },
+  {
+    id: "pancakeswap",
+    name: "PancakeSwap",
+    adjusted_volume_24h_share: 0.04787259,
+    fiats: [],
+  },
+  {
+    id: "deepcoin",
+    name: "Deepcoin",
+    adjusted_volume_24h_share: 0.03837866,
+    fiats: [],
+  },
+  {
+    id: "finexbox",
+    name: "FINEXBOX",
+    adjusted_volume_24h_share: 0.0383689,
+    fiats: [],
+  },
+  {
+    id: "bilaxy",
+    name: "Bilaxy",
+    adjusted_volume_24h_share: 0.03736779,
+    fiats: [],
+  },
+  {
+    id: "octium",
+    name: "Octium",
+    adjusted_volume_24h_share: 0.03706627,
+    fiats: [],
+  },
+  {
+    id: "indodax",
+    name: "Indodax",
+    adjusted_volume_24h_share: 0.0358311,
+    fiats: [
+      {
+        name: "Indonesian Rupiah",
+        symbol: "IDR",
+      },
+    ],
+  },
+  {
+    id: "apeswap",
+    name: "ApeSwap",
+    adjusted_volume_24h_share: 0.03479342,
+    fiats: [],
+  },
+  {
+    id: "currency-com",
+    name: "Currency.com",
+    adjusted_volume_24h_share: 0.03285461,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Polish Zloty",
+        symbol: "PLN",
+      },
+      {
+        name: "Turkish Lira",
+        symbol: "TRY",
+      },
+      {
+        name: "Hong Kong Dollar",
+        symbol: "HKD",
+      },
+      {
+        name: "Danish Krone",
+        symbol: "DKK",
+      },
+      {
+        name: "Norwegian Krone",
+        symbol: "NOK",
+      },
+      {
+        name: "Swedish Krona",
+        symbol: "SEK",
+      },
+    ],
+  },
+  {
+    id: "bitvavo",
+    name: "Bitvavo",
+    adjusted_volume_24h_share: 0.03245774,
+    fiats: [
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "txbit",
+    name: "Txbit",
+    adjusted_volume_24h_share: 0.02409441,
+    fiats: [],
+  },
+  {
+    id: "dexzbitz",
+    name: "Dexzbitz",
+    adjusted_volume_24h_share: 0.01395737,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+    ],
+  },
+  {
+    id: "hotbit",
+    name: "Hotbit",
+    adjusted_volume_24h_share: 0.01216096,
+    fiats: [],
+  },
+  {
+    id: "stex",
+    name: "STEX",
+    adjusted_volume_24h_share: 0.01114726,
+    fiats: [],
+  },
+  {
+    id: "btc6x-exchange",
+    name: "BTC6X Exchange",
+    adjusted_volume_24h_share: 0.00983612,
+    fiats: [],
+  },
+  {
+    id: "counos-exchange",
+    name: "Counos Exchange",
+    adjusted_volume_24h_share: 0.00856744,
+    fiats: [],
+  },
+  {
+    id: "tarmex",
+    name: "Tarmex",
+    adjusted_volume_24h_share: 0.00811703,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+    ],
+  },
+  {
+    id: "bitteam",
+    name: "BIT.TEAM",
+    adjusted_volume_24h_share: 0.00729949,
+    fiats: [],
+  },
+  {
+    id: "krypterz",
+    name: "Krypterz",
+    adjusted_volume_24h_share: 0.00727351,
+    fiats: [],
+  },
+  {
+    id: "patriot-exchange",
+    name: "Patriot Exchange",
+    adjusted_volume_24h_share: 0.00727351,
+    fiats: [],
+  },
+  {
+    id: "bittilo",
+    name: "Bittilo",
+    adjusted_volume_24h_share: 0.00727351,
+    fiats: [],
+  },
+  {
+    id: "bitunivex",
+    name: "Bitunivex",
+    adjusted_volume_24h_share: 0.00727351,
+    fiats: [],
+  },
+  {
+    id: "paintswap",
+    name: "PaintSwap",
+    adjusted_volume_24h_share: 0.00660086,
+    fiats: [],
+  },
+  {
+    id: "02x6",
+    name: "02x6",
+    adjusted_volume_24h_share: 0.00565837,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+    ],
+  },
+  {
+    id: "bakeryswap",
+    name: "Bakeryswap",
+    adjusted_volume_24h_share: 0.00248937,
+    fiats: [],
+  },
+  {
+    id: "probit-exchange",
+    name: "ProBit Exchange",
+    adjusted_volume_24h_share: 0.00229168,
+    fiats: [],
+  },
+  {
+    id: "litebiteu",
+    name: "LiteBit.eu",
+    adjusted_volume_24h_share: 0.00199625,
+    fiats: [
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "novadax",
+    name: "NovaDAX",
+    adjusted_volume_24h_share: 0.00193251,
+    fiats: [
+      {
+        name: "Brazil Real",
+        symbol: "BRL",
+      },
+    ],
+  },
+  {
+    id: "criptoswaps",
+    name: "Criptoswaps",
+    adjusted_volume_24h_share: 0.00191086,
+    fiats: [
+      {
+        name: "Turkish Lira",
+        symbol: "TRY",
+      },
+    ],
+  },
+  {
+    id: "xeggex",
+    name: "Xeggex",
+    adjusted_volume_24h_share: 0.00142636,
+    fiats: [],
+  },
+  {
+    id: "bololex",
+    name: "Bololex",
+    adjusted_volume_24h_share: 0.00114315,
+    fiats: [],
+  },
+  {
+    id: "coinswap-space",
+    name: "CoinSwap Space",
+    adjusted_volume_24h_share: 0.00110446,
+    fiats: [],
+  },
+  {
+    id: "bitopro",
+    name: "BitoPro",
+    adjusted_volume_24h_share: 0.00108419,
+    fiats: [
+      {
+        name: "Taiwan New Dollar",
+        symbol: "TWD",
+      },
+    ],
+  },
+  {
+    id: "bitladon",
+    name: "Bitladon",
+    adjusted_volume_24h_share: 0.00048356,
+    fiats: [
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+    ],
+  },
+  {
+    id: "cexio",
+    name: "CEX.IO",
+    adjusted_volume_24h_share: 0.00041012,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Euro",
+        symbol: "EUR",
+      },
+      {
+        name: "Pound Sterling",
+        symbol: "GBP",
+      },
+    ],
+  },
+  {
+    id: "bancor-network",
+    name: "Bancor Network",
+    adjusted_volume_24h_share: 0.00014332,
+    fiats: [],
+  },
+  {
+    id: "atomicdex",
+    name: "AtomicDEX",
+    adjusted_volume_24h_share: 0.00005422,
+    fiats: [],
+  },
+  {
+    id: "ionomy-exchange",
+    name: "Ionomy Exchange",
+    adjusted_volume_24h_share: 0.00004255,
+    fiats: [],
+  },
+  {
+    id: "hollaex",
+    name: "HollaEx",
+    adjusted_volume_24h_share: 0.00003161,
+    fiats: [],
+  },
+  {
+    id: "mcash-exchange",
+    name: "MCash Exchange",
+    adjusted_volume_24h_share: 0.00002874,
+    fiats: [],
+  },
+  {
+    id: "ninjaswap",
+    name: "NinjaSwap",
+    adjusted_volume_24h_share: 0.00001591,
+    fiats: [],
+  },
+  {
+    id: "btc-trade-ua",
+    name: "BTC Trade UA",
+    adjusted_volume_24h_share: 0.00001236,
+    fiats: [
+      {
+        name: "Ukrainian Hryvnia",
+        symbol: "UAH",
+      },
+    ],
+  },
+  {
+    id: "cryptex24",
+    name: "Cryptex24",
+    adjusted_volume_24h_share: 0.00001058,
+    fiats: [
+      {
+        name: "Ukrainian Hryvnia",
+        symbol: "UAH",
+      },
+    ],
+  },
+  {
+    id: "xbts",
+    name: "XBTS",
+    adjusted_volume_24h_share: 0.0000026,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Russian Ruble",
+        symbol: "RUB",
+      },
+      {
+        name: "Yuan Renminbi",
+        symbol: "CNY",
+      },
+    ],
+  },
+  {
+    id: "x7-exchange",
+    name: "X7 Exchange",
+    adjusted_volume_24h_share: 4.2e-7,
+    fiats: [
+      {
+        name: "US Dollars",
+        symbol: "USD",
+      },
+      {
+        name: "Brazil Real",
+        symbol: "BRL",
+      },
+    ],
+  },
+  {
+    id: "stellaswap",
+    name: "StellaSwap",
+    adjusted_volume_24h_share: 1.2e-7,
+    fiats: [],
+  },
+  {
+    id: "binance-dex",
+    name: "Binance DEX",
+    adjusted_volume_24h_share: 0,
+    fiats: [],
+  },
+  {
+    id: "occe",
+    name: "OCCE",
+    adjusted_volume_24h_share: 0,
+    fiats: [
+      {
+        name: "Ukrainian Hryvnia",
+        symbol: "UAH",
+      },
+    ],
+  },
+  {
+    id: "bitocto-exchange",
+    name: "Bitocto Exchange",
+    adjusted_volume_24h_share: 0,
+    fiats: [
+      {
+        name: "Indonesian Rupiah",
+        symbol: "IDR",
+      },
+    ],
+  },
+];
--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import searchTrending from "./mock/searchTrending";
 import search2 from "./mock/search2";
 import coinById from "./mock/coinById";
 import twitter from "./mock/twitter";
+import exchanges from "./mock/exchanges";
 import type { TrendingResponse } from "./api/coingecko/types";
 import type { SearchResponse } from "./api/coinpaprika/types";
 import type { GetCoinByIDResponse } from "./api/coinpaprika/types";
 import type { GetTwitterItem } from "./api/coinpaprika/types";
+import type { CoinExchangesItem } from "./api/coinpaprika/types";
 
 export function makeServer() {
   const server = new Server();
@@ -14,17 +16,17 @@ export function makeServer() {
   server.get("/search/trending", (): TrendingResponse => {
     return searchTrending;
   });
-
   server.get("/search", (): SearchResponse => {
     return search2;
   });
-
   server.get("/coins/:coin_id", (): GetCoinByIDResponse => {
     return coinById;
   });
-
   server.get("coins/:coin_id/twitter", (): Array<GetTwitterItem> => {
     return twitter;
+  });
+  server.get("coins/:coin_id/exchanges", (): Array<CoinExchangesItem> => {
+    return exchanges;
   });
 
   return server;
