95c69b91617c405e67dbcf4a3c57bc97dcc179da
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-17 07:09:41 +0600

    feat: add TagList component

---
+++ src/components/coin/TagList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+import type { TagItem } from "../../api/coinpaprika/types";
+
+defineProps({
+  links: Array as PropType<Array<TagItem>>,
+});
+</script>
+
+<template>
+  <ul class="list-unstyled d-flex flex-wrap">
+    <li v-for="item in links" :key="item.id" class="mr-2 mb-2">
+      <span class="badge badge-info text-uppercase">{{ item.name }}</span>
+    </li>
+  </ul>
+</template>
