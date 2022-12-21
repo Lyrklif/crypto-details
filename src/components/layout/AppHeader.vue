0efe5c3d0a98a4ac972a32bfc2dcf81fdf5e2fd6
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-21 15:01:23 +0600

    refactor: move LocaleChanger in /features

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
-import LocaleChanger from "../LocaleChanger.vue";
+import LocaleChanger from "../features/LocaleChanger.vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
