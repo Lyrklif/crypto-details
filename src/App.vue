27def973293159d8ba44549e9fec8c5b742bef86
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-23 08:46:36 +0600

    feat: add pagination

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import AppHeader from "./components/layout/header/AppHeader.vue";
+import Pagination from "./components/layout/Pagination.vue";
 </script>
 
 <template>
   <div class="d-flex flex-row-reverse">
     <AppHeader />
 
-    <main class="section container">
+    <main class="container">
+      <Pagination class="mt-4 mb-5" />
       <RouterView />
     </main>
   </div>
---
+++ src/components/layout/Pagination.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { homeLink } from "../../constants/navLinks";
+import { useI18n } from "vue-i18n";
+import { useRoute } from "vue-router";
+
+const { t } = useI18n();
+const route = useRoute();
+</script>
+
+<template>
+  <header v-if="route.name !== homeLink.name">
+    <ol class="breadcrumb breadcrumb-gray">
+      <li class="breadcrumb-item">
+        <RouterLink :to="{ name: homeLink.name }" :title="homeLink.text">
+          {{ t(homeLink.text) }}
+        </RouterLink>
+      </li>
+      <li class="breadcrumb-item active" aria-current="page">Data</li>
+    </ol>
+  </header>
+</template>
