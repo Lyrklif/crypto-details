f1051d3ddf650c40607c71ccf1fe216a6727982c
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-25 01:35:45 +0600

    chore: minify html

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,27 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 const base = process.env.NODE_ENV === "development" ? "/" : "/crypto-details/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueI18nPlugin({})],
+  plugins: [
+    vue(),
+    vueJsx(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: "Crypto details",
+          description:
+            "Detailed information about cryptocurrency using public api",
+        },
+      },
+    }),
+    VueI18nPlugin({}),
+  ],
   base,
   resolve: {
     alias: {
