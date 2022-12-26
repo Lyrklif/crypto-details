c8a32dd187ed9dfe6f236cf95ae671435f463e0c
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-26 23:32:10 +0600

    feat: [progress] add pagination

---
+++ src/components/base/PagePagination.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+defineProps({
+  count: {
+    type: Number as PropType<number>,
+    required: true,
+  },
+  routeName: {
+    type: String as PropType<string>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <nav>
+    <ol class="pagination circle-pagination">
+      <li class="page-item">
+        <a class="page-link" href="#"
+          ><i class="fas fa-angle-double-left"></i
+        ></a>
+      </li>
+
+      <li
+        class="page-item"
+        v-for="(_, index) in Array(count)"
+        :key="`pagination-${index}`"
+      >
+        <a class="page-link" href="#">1</a>
+        <RouterLink
+          :to="{ name: routeName, params: { page: index + 1 } }"
+          v-slot="{ isActive }"
+          :class="{ active: isActive }"
+        >
+          {{ index + 1 }}
+        </RouterLink>
+      </li>
+
+      <li class="page-item">
+        <a class="page-link" href="#"
+          ><i class="fas fa-angle-double-right"></i
+        ></a>
+      </li>
+    </ol>
+  </nav>
+</template>
