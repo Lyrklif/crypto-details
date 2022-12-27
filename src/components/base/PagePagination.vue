2782aadf8c1673fd59b55384d5221cf0238f7aac
Alexandr Kochurov <b2b4ly@gmail.com>
2022-12-27 22:47:39 +0600

    feat: pagination back, right btns

--- src/components/base/PagePagination.vue
+++ src/components/base/PagePagination.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { PropType } from "vue";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
 defineProps({
   count: {
     type: Number as PropType<number>,
@@ -16,32 +18,45 @@ defineProps({
 <template>
   <nav>
     <ol class="pagination circle-pagination">
-      <li class="page-item">
-        <a class="page-link" href="#"
-          ><i class="fas fa-angle-double-left"></i
-        ></a>
+      <li class="page-item" :class="{ disabled: +route.params.page <= 1 }">
+        <RouterLink
+          :to="{ name: routeName, params: { page: route.params.page - 1 } }"
+          class="page-link"
+        >
+          <i class="icon-angle-double-right left-icon" />
+        </RouterLink>
       </li>
 
       <li
-        class="page-item"
         v-for="(_, index) in Array(count)"
         :key="`pagination-${index}`"
+        class="page-item"
+        :class="{ active: +route.params.page === index + 1 }"
       >
-        <a class="page-link" href="#">1</a>
         <RouterLink
           :to="{ name: routeName, params: { page: index + 1 } }"
           v-slot="{ isActive }"
           :class="{ active: isActive }"
+          class="page-link"
         >
           {{ index + 1 }}
         </RouterLink>
       </li>
 
-      <li class="page-item">
-        <a class="page-link" href="#"
-          ><i class="fas fa-angle-double-right"></i
-        ></a>
+      <li class="page-item" :class="{ disabled: +route.params.page >= count }">
+        <RouterLink
+          :to="{ name: routeName, params: { page: +route.params.page + 1 } }"
+          class="page-link"
+        >
+          <i class="icon-angle-double-right" />
+        </RouterLink>
       </li>
     </ol>
   </nav>
 </template>
+
+<style scoped>
+.left-icon {
+  transform: rotate(180deg);
+}
+</style>
