a81cc2fdf1e0cded738a524c6cca1bf55ccc3c1e
Alexandr Kochurov <b2b4ly@gmail.com>
2023-04-29 11:15:26 +0400

    feat: add webfontDownload plugin for fonts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+// @ts-ignore
+import replaceAllInserter from "string.prototype.replaceall";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import { createHtmlPlugin } from "vite-plugin-html";
+import webfontDownload from "vite-plugin-webfont-dl";
 
+replaceAllInserter.shim();
 const base = process.env.NODE_ENV === "development" ? "/" : "/crypto-details/";
 
 // https://vitejs.dev/config/
@@ -15,6 +19,9 @@ export default defineConfig({
     vueJsx(),
     createHtmlPlugin({ minify: true }),
     VueI18nPlugin({}),
+    webfontDownload([
+      "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&display=swap",
+    ]),
   ],
   base,
   resolve: {
