e1d4b0282c1466f2c8f0296359f0dd0f5fe2c675
Alexandr Kochurov <b2b4ly@gmail.com>
2023-05-14 21:42:48 +0400

    feat: (process) move to primevue

--- src/components/features/search/error/NotFound.vue
+++ src/components/features/search/error/NotFound.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
+import Message from "primevue/message";
 
 const { t } = useI18n();
 const emit = defineEmits(["close"]);
 </script>
 
 <template>
-  <div
-    class="alert alert-danger alert-dismissible shadow-soft fade show"
-    role="alert"
-  >
-    <span class="alert-inner--text">{{ t("errors.token_not_found") }}</span>
-    <button
-      type="button"
-      class="close"
-      data-dismiss="alert"
-      :aria-label="t('search.close')"
-      :title="t('search.close')"
-      @click="emit('close')"
-    >
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
+  <Message severity="error" @close="emit('close')">
+    {{ t("errors.token_not_found") }}
+  </Message>
 </template>
